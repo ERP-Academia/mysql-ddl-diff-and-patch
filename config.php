dd6d9f3d3293404880133fca18d3931b17a8a19c
ERP-Academia <59137555+ERP-Academia@users.noreply.github.com>
2020-04-11 04:11:10 +0300

    Read me update

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 $whereis_mysqldump = '/usr/bin/mysqldump';
 $whereis_mysql = '/usr/bin/mysql';
 
-$mysql_user = 'root2';
-$mysql_pass = '666';
+$mysql_user = 'root';
+$mysql_pass = '';
 
 $new_db = 'diff_new';
 $old_db = 'diff_old';
